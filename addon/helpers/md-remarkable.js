0d6fa6e672c00c917554faee90617b183d652f8b
John Otander <johnotander@gmail.com>
2014-11-26 20:18:26 -0700

    Add initial functionality

--- addon/helpers/md-remarkable.js
+++ addon/helpers/md-remarkable.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
-export function mdRemarkable(input) {
-  return input;
+export function mdRemarkable(markdownInput) {
+  var md = new Remarkable();
+  return new Ember.Handlebars.SafeString(md.render(markdownInput));
 }
 
 export default Ember.Handlebars.makeBoundHelper(mdRemarkable);
