a93d123b46f8add80d11751dafe13558c32a1758
John Otander <johnotander@gmail.com>
2014-12-11 21:02:56 -0700

    Break out component into the addon directory

--- app/components/md-text.js
+++ app/components/md-text.js
@@ -1,22 +1,3 @@
-import Ember from 'ember';
-import Remarkable from 'remarkable';
+import mdText from 'ember-remarkable/components/md-text';
 
-export default Ember.Component.extend({
-  text: '',
-  typographer: false,
-  linkify: false,
-
-  parsedMarkdown: function() {
-    var md = new Remarkable(this.get('buildOptions'));
-    var html = md.render(this.get('text'));
-
-    return new Ember.Handlebars.SafeString(html);
-  }.property('text'),
-
-  buildOptions: function() {
-    return {
-      typographer: this.get('typographer'),
-      linkify: this.get('linkify')
-    };
-  }.property('typographer', 'linkify', 'html')
-});
+export default mdText;
