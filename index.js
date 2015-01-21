9355287669fc474d2a77669662fa0650dbe6127f
John Otander <johnotander@gmail.com>
2015-01-20 22:56:44 -0700

    Add highlightjs and hljs shim

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     this.app.import(app.bowerDirectory + '/remarkable/dist/remarkable.js');
+    this.app.import(app.bowerDirectory + '/highlightjs/highlight.pack.js');
     this.app.import('vendor/ember-remarkable/shim.js', {
       type: 'vendor',
       exports: { 'remarkable': ['default'] }
+    });
+    this.app.import('vendor/ember-remarkable/highlightjs-shim.js', {
+      type: 'vendor',
+      exports: { 'hljs': ['default'] }
     });
   }
 };
